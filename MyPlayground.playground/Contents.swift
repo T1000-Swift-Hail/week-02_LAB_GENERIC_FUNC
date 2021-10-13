ef8900c9323de4e7eba546fffe47e1423945da18
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-13 10:15:33 +0300

    Done Generic

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+func pairInArray<T>(a: T ,b: T ) -> [T]{
+    
+    return [a, b]
+}
+
+let pairs = pairInArray(a: [5 , 7], b: [23,76])
+
+print(pairs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let ArrayPair = pairs([])
