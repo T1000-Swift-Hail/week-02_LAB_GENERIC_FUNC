41ba212ed6a621cb009cc324c58bf440d27a31bb
Anas Hamad <anashamad@MacBook-Air-Anas.local>
2021-10-14 20:05:45 +0300

    Done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-var greeting = "Hello, playground"
+    
+    func pairInArray <T> ( a:T, b:T)-> [T]{
+        return  [a,b]
+}
+
+
+let pairs = pairInArray(a: [1,2,3,4,5,], b: [1,2,4,2,1,7])
+print(pairs)
